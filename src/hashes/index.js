c6e7adcfd7e9d6bd150fea2d09598c8d266c29b2
Christian Schmitz <cschmitz398@gmail.com>
2024-10-12 20:23:51 -0600

    ledger-babbage lib upgrade

--- src/hashes/index.js
+++ src/hashes/index.js
@@ -14,8 +14,12 @@ export {
  * @typedef {import("@helios-lang/ledger-babbage").MintingPolicyHashLike} MintingPolicyHashLike
  * @typedef {import("@helios-lang/ledger-babbage").PubKeyHashLike} PubKeyHashLike
  * @typedef {import("@helios-lang/ledger-babbage").ScriptHashLike} ScriptHashLike
- * @typedef {import("@helios-lang/ledger-babbage").StakingHashKind} StakingHashKind
  * @typedef {import("@helios-lang/ledger-babbage").StakingHashLike} StakingHashLike
  * @typedef {import("@helios-lang/ledger-babbage").StakingValidatorHashLike} StakingValidatorHashLike
  * @typedef {import("@helios-lang/ledger-babbage").ValidatorHashLike} ValidatorHashLike
+ */
+
+/**
+ * @template [Context=unknown]
+ * @typedef {import("@helios-lang/ledger-babbage").StakingHashI<Context>} StakingHashI
  */
